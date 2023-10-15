2132e682304e13fae89a60a8b6c1374b0d45c88b
Arman74400 <146393881+Arman74400@users.noreply.github.com>
2023-10-15 12:17:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,19 +23,6 @@ bot = Client(
            api_id=API_ID,
 
            api_hash=API_HASH,
-
-           string=STRING,
-
+           
            plugins=dict(root='plugins'))
            
-
-if STRING:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
